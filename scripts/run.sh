5d7cb1e19153b3ba70c24f1eb5e3d728881a25c4
TraiLoveLinh <traipupgmobile@gmail.com>
2024-07-30 21:51:30 +0700

    add mouse accel speed, reduce it sir

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,9 @@ picom &
 ibus-daemon &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 
-feh --bg-fill ~/Pictures/Wallpapers/ayu-mirage.png 
-#feh --bg-fill ~/Pictures/Wallpapers/aihh.jpg 
+#feh --bg-fill ~/Pictures/Wallpapers/ayu-mirage.png 
+feh --bg-fill ~/Pictures/Wallpapers/minimal_landscape.jpg
+xinput --set-prop 8 "libinput Accel Speed" -0.8
 
 dash ~/.config/chadwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
