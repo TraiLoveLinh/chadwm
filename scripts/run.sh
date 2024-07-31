002da7fe86c02686dc1048a7a1eaf7f6a5f80129
TraiLoveLinh <traipupgmobile@gmail.com>
2024-07-31 18:41:48 +0700

    add touch pad tap to click and natural scrolling

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,8 @@ ibus-daemon &
 #feh --bg-fill ~/Pictures/Wallpapers/ayu-mirage.png 
 feh --bg-fill ~/Pictures/Wallpapers/minimal_landscape.jpg
 xinput --set-prop 8 "libinput Accel Speed" -0.8
+xinput set-prop  "DELL0AB0:00 04F3:3147 Touchpad" "libinput Tapping Enabled" 1
+xinput --set-prop "DELL0AB0:00 04F3:3147 Touchpad" 'libinput Natural Scrolling Enabled' 1
 
 dash ~/.config/chadwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
